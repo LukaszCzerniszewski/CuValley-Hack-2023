63054860e27393016e5dfed8662150625a6be167
Celcik <lukasz.czerniszewski@gmail.com>
2023-01-25 21:44:00 +0100

    Dodanie funckji do konwersji csv to json dla danych z witryny

--- data/dataImport.py
+++ data/dataImport.py
@@ -1,16 +1,16 @@
 import pandas
 
 
-
+#Wczytanie testowych danych Hydro przekazanych w excelu
 def loadHydroAsJson ():
     excel_data_df = pandas.read_excel('hydro - sample.xlsx',
                                       sheet_name='hydro',
                                       skiprows=[0, 1],
                                       usecols=[0,1,2],
                                       names=['Data','151160060','150180060'])
-
     return (excel_data_df.to_json(orient='records'))
 
+#Wczytanie testowych danych Meto przekazanych w excelu
 def loadMetoAsJson ():
     excel_data_df = pandas.read_excel(
         'meteo - sample.xlsx',
@@ -21,8 +21,27 @@ def loadMetoAsJson ():
                '250160030','250160070','250170050','251160230',
                '251160170']
     )
-
     return (excel_data_df.to_json(orient='records'))
 
-print(loadMetoAsJson ())
-print(loadHydroAsJson())
+#Funckja do ladowania nowych danych hydro pobranych jako csv ze strony
+def loadDataFromPageCsv():
+    columns = ['kod_stacji', 'rok', 'miesiac', 'dzien', 'opad[mm]']
+    df = pandas.read_csv('pobraneDane.csv', header=1,
+                         usecols=[0,2,3,4,5],
+                          names=columns,
+                          skiprows=0,
+                         encoding= 'unicode_escape')
+
+    #ujednolicenie do daty przekazanego przez KGHM
+    df["Data"] = df['dzien'].astype(str) +"-"+ df["miesiac"].astype(str)+"-"+ df["rok"].astype(str)
+    df = df.drop(['dzien', "miesiac", "rok"], axis=1)
+
+    #zmiana do stringa
+    df["kod_stacji"] = df["kod_stacji"].astype(str)
+    df["opad[mm]"] = df["opad[mm]"] .astype(str)
+
+    return df.to_json(orient='records')
+
+# print(loadMetoAsJson ())
+# print(loadHydroAsJson())
+#print(loadDataFromPageCsv())
